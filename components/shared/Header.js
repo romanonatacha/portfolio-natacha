e59a7e3a9b193024572e4575e0d81ba8ffc68de6
Natacha Romano <natacharomanonr@gmail.com>
2020-05-30 22:14:14 -0300

    mobile menu

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 
 export default class Header extends Component {
     render() {
-        const { title, children, handleClick } = this.props
+        const { handleClick } = this.props
 
         const menu = [
             {
@@ -31,7 +31,7 @@ export default class Header extends Component {
                     <div className="header_left white">
                         <div className="text-right padding32 font-xl x-bold">NATACHA ROMANO</div>
                     </div>
-                    <div className="header_right text-right bold">
+                    <div className="header_right text-right light">
                         {menu.map((item, i) => (
                             <Link key={i} href={item.url}>
                                 <a className="padding16">{item.title}</a>
@@ -39,34 +39,26 @@ export default class Header extends Component {
                         ))}
                         <div className="padding16 inline">
                             <button className="toggle-mode" onClick={handleClick}>
-                                <i class="fas fa-moon"></i>
-                                <i class="fas fa-sun"></i>
+                                <i className="fas fa-moon"></i>
+                                <i className="fas fa-sun"></i>
                             </button>
                         </div>
 
                     </div>
                 </div>
 
-                <Navbar className="dt_hide tb_hide" collapseOnSelect expand="lg" bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar className="dt_hide tb_hide header-mb" collapseOnSelect expand="lg">
+                    <Navbar.Brand href="#home" className="font-lg x-bold">NATACHA ROMANO</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <button className="toggle-mode" onClick={handleClick}>
+                        <i className="fas fa-moon"></i>
+                        <i className="fas fa-sun"></i>
+                    </button>
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
-                             </Nav.Link>
+                        <Nav className="mr-auto light">
+                            {menu.map((item, i) => (
+                                <Nav.Link key={i} href={item.url}>{item.title}</Nav.Link>
+                            ))}
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
---
+++ pages/skills.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import BaseLayout from '../components/layouts/BaseLayout'
+
+export default class Skills extends Component {
+    render() {
+        return (
+            <BaseLayout>
+                <div id="words">
+                    <div id="word1" className="word Foreground">Hey</div>
+                    <div id="word2" className="word Background">World!</div>
+                    <div id="word3" className="word Foreground">This</div>
+                    <div id="word4" className="word Background">is</div>
+                    <div id="word5" className="word Foreground">Word</div>
+                    <div id="word6" className="word Background">Floating</div>
+                    <div id="word7" className="word Foreground">Space</div>
+                    <div id="word8" className="word Background"> Build</div>
+                    <div id="word9" className="word Background">by</div>
+                    <div id="word10" className="word Foreground">Bhargav</div>
+                    <div id="word11" className="word Background">Gandhi</div>
+                    <div id="word12" className="word Background">Try</div>
+                    <div id="word13" className="word Background">Entering</div>
+                    <div id="word14" className="word Background">One</div>
+                    <div id="word15" className="word Foreground">Word</div>
+                </div>
+            </BaseLayout>
+        )
+    }
+}
