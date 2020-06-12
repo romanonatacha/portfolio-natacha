813cec28a98912333701b7b58631a3b83b216021
Natacha Romano <natacharomanonr@gmail.com>
2020-06-12 00:02:13 -0300

    homepage

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ export default class Index extends Component {
         return (
             <BaseLayout title="Natacha Romano" pageClass="index">
                 <div className="boxed">
+                    <div class="typewriter default">
+                        <h2>front-end engineer</h2>
+                        <h2>css specialist</h2>
+                    </div>
                     <img className="rocket" src="/images/rockets.svg" />
                 </div>
             </BaseLayout>
