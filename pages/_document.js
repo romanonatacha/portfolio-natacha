dbf60f135a57e127d58118963c87ca8eea27a469
Natacha Romano <natacharomanonr@gmail.com>
2020-06-13 19:00:56 -0300

    metatags

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,15 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/images/favicon.ico" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="natacha romano | portfolio" />
+          <meta property="og:description" content="front-end developer, software engineer, css specialist and web designer." />
+          <meta property="og:url" content="https://romanonatacha.github.io" />
+          <meta property="og:image" content="/images/thumbnail2.png" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="627" />
+          <meta name="twitter:image" content="/images/thumbnail.png" />
         </Head>
         <body>
           <Main />
