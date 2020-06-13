ccac495251122b4eed303cb489634967dfb38bea
Natacha Romano <natacharomanonr@gmail.com>
2020-06-13 19:22:23 -0300

    fix metatags

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,10 @@ class MyDocument extends Document {
           <meta property="og:image:type" content="image/png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="627" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="https://romanonatacha.github.io" />
+          <meta name="twitter:title" content="natacha romano | portfolio" />
+          <meta name="twitter:description" content="front-end developer, software engineer, css specialist and web designer." />
           <meta name="twitter:image" content="/images/thumbnail.png" />
         </Head>
         <body>
