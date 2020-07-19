21e6854663d2cbc927b293f0110022d7f32d3b95
Natacha Romano <natacharomanonr@gmail.com>
2020-07-19 00:06:47 -0300

    adding upworking url on contact bar

--- components/shared/SocialBar.js
+++ components/shared/SocialBar.js
@@ -8,6 +8,9 @@ const SocialBar = () => {
                 <a href="https://www.linkedin.com/in/natacha-romano/" target="_blank"><i className="icon fab fa-linkedin"></i><span>LinkedIn</span></a>
             </li>
             <li>
+                <a href="https://www.upwork.com/fl/romanonatacha" target="_blank"><i className="icon fas fa-laptop-house"></i><span>upwork</span></a>
+            </li>
+            <li>
                 <a href="mailto:natacharomanonr@gmil.com" target="_blank"><i className="icon fas fa-envelope"></i><span>Email</span></a>
             </li>
         </ul>
