28b5d4f40d4803215a50deb71fa5ecc73ed31b0d
Natacha Romano <natacharomanonr@gmail.com>
2021-01-08 18:30:38 -0300

    updating upwork link

--- components/shared/SocialBar.js
+++ components/shared/SocialBar.js
@@ -8,10 +8,10 @@ const SocialBar = () => {
                 <a href="https://www.linkedin.com/in/natacha-romano/" target="_blank"><i className="icon fab fa-linkedin"></i><span>LinkedIn</span></a>
             </li>
             <li>
-                <a href="https://www.upwork.com/fl/romanonatacha" target="_blank"><i className="icon fas fa-laptop-house"></i><span>Hire me on upwork</span></a>
+                <a href="https://www.upwork.com/ab/flservices/cl/public/org/1173655290649358337" target="_blank"><i className="icon fas fa-laptop-house"></i><span>Hire me on upwork</span></a>
             </li>
             <li>
-                <a href="mailto:natacharomanonr@gmil.com" target="_blank"><i className="icon fas fa-envelope"></i><span>Email</span></a>
+                <a href="mailto:natacharomanonr@gmail.com" target="_blank"><i className="icon fas fa-envelope"></i><span>Email</span></a>
             </li>
         </ul>
     )
